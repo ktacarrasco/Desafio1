ab123d6111707b971c9b7ba2de6f41aaf28fbeba
kta <kta.carrasco@gmail.com>
2020-02-21 15:28:20 -0300

    desafio01

--- app/src/main/java/com/example/myapplicationdesafio1/MainActivity.java
+++ app/src/main/java/com/example/myapplicationdesafio1/MainActivity.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView miimagen = findViewById(R.id.miimagen);
+        Picasso.get().load("https://sendeyo.com/up/d/6be1254bc4").into(miimagen);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Presione el Boton", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Usuario ingresado con éxito", Toast.LENGTH_SHORT).show();
                 intentExample();
             }
         });
